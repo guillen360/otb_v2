54f8100bb5effd53a546600d8321e22e39494652
lcg <guillen360@gmail.com>
2012-08-22 14:05:56 -0500

    added admin css files to project's static folder

--- otb_v2/settings.py
+++ otb_v2/settings.py
@@ -13,14 +13,7 @@ MANAGERS = ADMINS
 
 sqliteDB =  os.path.join(os.getcwd(), 'otb.db')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': sqliteDB,                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+
 }
 
 # Local time zone for this installation. Choices can be found here:
